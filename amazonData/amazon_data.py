19766496b6a33b202f6d3f2c9f710c012bb9dc18
samansaeedi <ssaeedi1367@gmail.com>
2015-04-24 14:00:58 -0700

    added a simple program to parse amazon data

---
+++ amazonData/amazon_data.py
@@ -0,0 +1,21 @@
+import gzip
+import simplejson
+
+def parse(filename):
+  f = gzip.open(filename, 'r')
+  entry = {}
+  for l in f:
+    l = l.strip()
+    colonPos = l.find(':')
+    if colonPos == -1:
+      yield entry
+      entry = {}
+      continue
+    eName = l[:colonPos]
+    rest = l[colonPos+2:]
+    entry[eName] = rest
+  yield entry
+
+for e in parse("../Video_Games.txt.gz"):
+  print simplejson.dumps(e)
+  break #just display the first entry
