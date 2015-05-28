c1a1846d858fc3b0e13ef7bc7558dd84d60fbd48
Marvin <reacxtion@gmail.com>
2015-05-27 22:07:58 -0700

    Updated to limit product ids

---
+++ item_item_cf.py
@@ -0,0 +1,37 @@
+import pymongo
+
+def get_products(collection, products_dict):
+    unique_p = []
+
+    if (collection.count() > 10000000):
+        db.subset.drop()
+        for data in collection.find().limit(500000):
+            db.subset.insert_one(data)
+        unique_p = list(db.subset.distinct("product/productId"))
+    else:
+        unique_p = list(collection.distinct("product/productId"))
+
+    db.subset.drop()
+
+    unique_p = list(collection.distinct("product/productId"))
+    for product in unique_p:
+        products_dict[product] = {}
+
+if __name__ == '__main__':
+    client = pymongo.MongoClient()
+    db = client.cs594
+    
+    game_products_dict = {}
+    music_products_dict = {}
+    movie_products_dict = {}
+    book_products_dict = {}
+    
+    get_products(db.games, game_products_dict)
+    get_products(db.music, music_products_dict)
+    get_products(db.movies, movie_products_dict)
+    get_products(db.books, book_products_dict)
+
+    print "Games unique ids: ", len(game_products_dict)
+    print "Music unique ids: ", len(music_products_dict)
+    print "Movies unique ids: ", len(movie_products_dict)
+    print "Books unique ids: ", len(book_products_dict)
